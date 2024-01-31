33b9451319bdf99a585fdaa810938f6c81d853bd
zhgzhg <perkoz@mail.bg>
2024-01-31 12:25:07 +0000

    Deploying to gh-pages from  @ 547328f375998c663fd88f9b9f1143abc281de6d 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -50,11 +50,11 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "_a_f_s_k_8h_source.html",
-"class_module.html#a34927454be3a34df8a5a586c46c428af",
-"class_radio_lib_hal.html#a6b5341c59728a1932e33216feee04ea5",
-"class_s_x1279.html#a324a37dee0522f43692cd414141becc2",
-"classn_r_f24.html#a31bcc5a8c3747bf08a273dbdadc5481a",
-"struct_lo_ra_w_a_n_band__t.html"
+"class_module.html#a081191402ad5c49e6c21982fc4587b77",
+"class_radio_lib_hal.html#a467356d0f1b097733da5ba99884cf354",
+"class_s_x1278.html#aa57b713988cfa224a6db2ff325052931",
+"classn_r_f24.html#a0429a9d6524005065e6fac21aaebdcbf",
+"struct_bell_modem__t.html#ac57de3660aafb52bcf1b3197380c6ce5"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
